20c324c235a7981e47f49522f878c1b2f780a284
kijimaru01 <suravut2539@hotmail.com>
2020-08-28 01:21:35 +0700

    update28/8/2563 update webcam streamming

--- index.js
+++ index.js
@@ -1,53 +1,25 @@
-const express = require('express')
-const app = express()
-const moment = require('moment')
-const port = 5000
+var app = require("express")();
+var http = require("http").Server(app);
+var io = require("socket.io")(http);
+var port = process.env.PORT || 3000;
+const path = require("path");
+const cv = require("opencv4nodejs");
 
-const route_api = require('./api')
+const wCap = new cv.VideoCapture(0);
+const FPS = 30;
+wCap.set(cv.CAP_PROP_FRAME_WIDTH, 300);
+wCap.set(cv.CAP_PROP_FRAME_HEIGHT, 300);
 
+app.get("/", function (req, res) {
+  res.sendFile(path.join(__dirname + "/index.html"));
+});
 
-app.use((req, res, next) => {
-    console.log(`LOGGED:${moment().format('MMMM Do YYYY,h:mm:ss a')}`)
-    next()
-  })
-  app.use('/api', route_api)
-  app.get('/', (req, res) => {
-    res.send("Home")
-  })
-  app.get('/about', (req, res) => {
-    res.send("<h1>เจ้าต้าวอ้วงงงง</h1>")
-  })
+setInterval(() => {
+  const frame = wCap.read();
+  const image = cv.imencode(".jpg", frame).toString("base64");
+  io.emit("image", image);
+}, 1000 / FPS);
 
-  
-  app.post('/', function (req, res) {
-    res.send('Got a POST request')
-  })
-  app.put('/user', function (req, res) {
-    res.send('Got a PUT request at /user')
-  })
-  app.delete('/user', function (req, res) {
-    res.send('Got a DELETE request at /user')
-  })
-// app.get('/', (req, res) => 
-// res.send('res นี่คือการส่งกลับไปยัง Browser')
-
-// )
-
-
-//รับ parameter แบบ Query string
-// GET
-// localhost:5000/student?code=1234&x=5&y=8
-app.get('/student', (req, res) => {
-  console.log(req.query);
-  res.send("<h1>Student Query string</h1> " + req.query.code )
-})
-
-// รับ parameter แบบ Url Params
- app.get('/student/:code', (req, res) => {
-  console.log(req.query);
-  console.log(req.params);
-  res.send("<h1>Student url parem</h1> " + req.params.code )
-})
-
-
-app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
+http.listen(port, function () {
+  console.log("listening on:" + port);
+});
