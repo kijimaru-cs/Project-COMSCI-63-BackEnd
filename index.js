84e08fc99716f53bda695c7ef397b70e03995130
kijimaru01 <suravut2539@hotmail.com>
2020-09-17 00:23:34 +0700

    socket and stream

--- index.js
+++ index.js
@@ -1,24 +1,55 @@
 var app = require("express")();
+const express = require("express");
 var http = require("http").Server(app);
 var io = require("socket.io")(http);
-var port = process.env.PORT || 3000;
-const path = require("path");
-const cv = require("opencv4nodejs");
+var port = process.env.PORT || 3001;
+app.use("/static", express.static("./static/"));
+let broadcasterVideo;
+let broadcasterAudio;
 
-const wCap = new cv.VideoCapture(0);
-const FPS = 30;
-wCap.set(cv.CAP_PROP_FRAME_WIDTH, 300);
-wCap.set(cv.CAP_PROP_FRAME_HEIGHT, 300);
-
-app.get("/", function (req, res) {
-  res.sendFile(path.join(__dirname + "/index.html"));
+io.sockets.on("error", (e) => console.log(e));
+io.sockets.on("connection", (socket) => {
+  socket.on("broadcasterVideo", () => {
+    broadcasterVideo = socket.id;
+    socket.broadcast.emit("broadcasterVideo");
+  });
+  socket.on("broadcasterAudio", () => {
+    broadcasterAudio = socket.id;
+    socket.broadcast.emit("broadcasterAudio");
+  });
+  socket.on("watcherVideo", () => {
+    socket.to(broadcasterVideo).emit("watcherVideo", socket.id);
+  });
+  socket.on("watcherAudio", () => {
+    socket.to(broadcasterAudio).emit("watcherAudio", socket.id);
+  });
+  socket.on("offerVideo", (id, message) => {
+    socket.to(id).emit("offerVideo", socket.id, message);
+  });
+  socket.on("offerAudio", (id, message) => {
+    socket.to(id).emit("offerAudio", socket.id, message);
+  });
+  socket.on("answerVideo", (id, message) => {
+    socket.to(id).emit("answerVideo", socket.id, message);
+  });
+  socket.on("answerAudio", (id, message) => {
+    socket.to(id).emit("answerAudio", socket.id, message);
+  });
+  socket.on("candidateVideo", (id, message) => {
+    socket.to(id).emit("candidateVideo", socket.id, message);
+  });
+  socket.on("candidateAudio", (id, message) => {
+    socket.to(id).emit("candidateAudio", socket.id, message);
+  });
+  socket.on("disconnect", () => {
+    socket.to(broadcasterVideo).emit("disconnectPeer", socket.id);
+    socket.to(broadcasterAudio).emit("disconnectPeer", socket.id);
+  });
+  socket.on("sendMessage", (msg) => {
+    console.log(msg);
+    io.emit("sendMessage", msg);
+  });
 });
-
-setInterval(() => {
-  const frame = wCap.read();
-  const image = cv.imencode(".jpg", frame).toString("base64");
-  io.emit("image", image);
-}, 1000 / FPS);
 
 http.listen(port, function () {
   console.log("listening on:" + port);
