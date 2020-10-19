ce326a3f07cb017a1743f3c9cdcfdbcf8841c210
kijimaru01 <suravut2539@hotmail.com>
2020-10-19 11:56:06 +0700

    fix acao

--- index.js
+++ index.js
@@ -12,12 +12,9 @@ let broadcasterAudio;
 user = [];
 userAll = [];
 app.use(cors())
-app.use((req, res, next) => {
-      res.header('Access-Control-Allow-Origin', '*')
-      res.header('Access-Control-Allow-Methods','POST, GET, PUT, PATCH, DELETE, OPTIONS')
-      res.header('Access-Control-Allow-Headers','Content-Type, Option, Authorization')
-      return next()
-   })
+app.get('/', (req, res)=> {
+  res.set('Access-Control-Allow-Origin', '*')
+})
 
 
 io.sockets.on("error", (e) => console.log(e));
